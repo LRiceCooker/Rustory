00045460a2a4ca5e4a21fa582b59e5a91c17e05d
Mathys <mathys@MacBook-Pro-de-Hospitalink.local>
2021-07-27 16:46:52 +0200

    feat(wording) start implement json parsing

--- src/text/wording.rs
+++ src/text/wording.rs
@@ -1,4 +1,14 @@
+use std::fs;
+use serde_json::{Result, Value};
 
-pub fn get() -> &'static str{
-    return "comming soon";
+ fn get_raw_data () -> String {
+    let raw_data = fs::read_to_string("./assets/wording/eng.json")
+        .expect("Something went wrong reading the file");
+    return raw_data;
+}
+
+pub fn get() {
+    let result = serde_json::from_str(&get_raw_data ());
+   
+    return ();
 }
