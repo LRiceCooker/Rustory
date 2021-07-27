0a070a02a0a4a6f833481dddef18c091dbbaea36
mathys <mathys.dupont.37@gmail.com>
2021-07-27 22:43:47 +0200

    feat(wording) start impl nested structs

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod text;
 
 fn main() { 
     //text::display::success("Hello, world!".to_string());
-    text::display::success(text::wording::get()["homeScreen"]["title"])
+    text::display::success(text::wording::get().homeScreen.title.to_string())
 }
