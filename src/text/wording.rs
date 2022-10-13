efc6d26bb6eaf1d8e3bc43e61ea2040ead4f64a4
Mathys Dupont <mathys.dupont@wttj.co>
2022-10-13 15:39:23 +0200

    chore: use toml file to settings

--- src/text/wording.rs
+++ src/text/wording.rs
@@ -1,16 +1,14 @@
 use std::{fs};
+use toml::Value;
 
-#[path="../types/wording/mod.rs"]
-mod wording;
 
- fn get_raw_data () -> String {
-    let raw_data = fs::read_to_string("./assets/wording/eng.json")
-        .expect("Something went wrong reading the file");
-    return raw_data;
-}
+pub fn lang(lang: &str) -> Value {
 
-pub fn get() -> wording::Wording {
-    let result: wording::Wording = serde_json::from_str(&get_raw_data()).expect("Unable to parse"); //c'est sencé marcher 
-     
-    return result;
+    let en = fs::read_to_string("./assets/wording/eng.toml").expect("").parse::<Value>().unwrap();
+
+    match lang {
+        "en" => return en,
+        _ => return en
+    }
+
 }
