76ed684def74331023c3cfe6aabde26f8211fed0
RiceCooker-dev <mathys.dupont.37@gmail.com>
2022-10-14 16:51:57 +0200

    feat: user input

--- src/input/text.rs
+++ src/input/text.rs
@@ -1,6 +1,26 @@
-use inquire::Text;
+use inquire::{Text, CustomUserError};
+
+#[path = "../text/wording.rs"]
+mod wording;
+
+#[path = "../commands/mapping.rs"]
+mod mapping;
+
+fn suggester(val: &str) -> Result<Vec<String>, CustomUserError> {
+    let val_lower = val.to_lowercase();
+    Ok(
+        mapping::BASE
+        .iter()
+        .filter(|s| s.to_lowercase().contains(&val_lower))
+        .map(|s| String::from(*s))
+        .collect()
+    )
+}
 
 pub fn get() {
-    let user_input = Text::new(">").prompt().unwrap();
+    let user_input = Text::new("rustory >")
+        .with_autocomplete(&suggester)
+        .prompt()
+        .unwrap();
     println!("{}", user_input);
 }
