f31eb533a35d5158307ec608c2b67a52e1c2fd01
RiceCooker-dev <mathys.dupont.37@gmail.com>
2022-10-14 16:10:03 +0200

    feat: add user input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod text;
 mod map;
+mod input;
 
 fn main() { 
     text::display::primary(text::wording::lang()["home_screen"]["title"].to_string());
     text::display::success("█".to_string());
+    input::text::get();
     //map::map_loader::load();
 }
