d34e700bdf73898596cb07b8d579e7b036471b82
RiceCooker-dev <mathys.dupont.37@gmail.com>
2022-10-15 02:43:04 +0200

    feat: roll command

--- src/commands/mapping.rs
+++ src/commands/mapping.rs
@@ -1,11 +1,13 @@
 
-pub const BASE: [&str; 2] = [
+pub const BASE: [&str; 3] = [
     "help", 
     "quit",
+    "roll",
 ];
 pub mod COMMANDS {
     use super::BASE;
 
     pub const HELP:&str = BASE[0];
     pub const QUIT:&str = BASE[1];
+    pub const ROLL:&str = BASE[2];
 }
