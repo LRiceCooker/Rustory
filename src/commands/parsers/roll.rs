385abe6c53b41994613736a88473de2117bdbf97
RiceCooker-dev <mathys.dupont.37@gmail.com>
2022-10-15 02:53:53 +0200

    chore: remove comments

--- src/commands/parsers/roll.rs
+++ src/commands/parsers/roll.rs
@@ -12,18 +12,14 @@ pub fn parse(args: Vec<&str>) -> Result<Roll, ()>{
         let mut dice: i8 = 0;
         let mut value: i8 = 0; 
         let mut add: i16 = 0;
-        //assert!(extarcted_param.contains('d'));
         let splited_param = extarcted_param.split('d').collect::<Vec<&str>>();
-        //assert!(splited_param[0].parse::<i8>().is_ok());
         dice = splited_param[0].parse::<i8>().expect("");
-        //assert!(splited_param.len()==2);
         if splited_param[1].parse::<i8>().is_ok() {
             value = splited_param[1].parse::<i8>().expect("");
             add = 0;
         }
         else {
-            //assert!(splited_param[1].contains('+') || splited_param[1].contains('-'));
-            let value_and_bonnus: Vec<&str>;//ternary + modifier sign
+            let value_and_bonnus: Vec<&str>;
             let mut operator = "";
             if splited_param[1].contains('+') {
                 value_and_bonnus = splited_param[1].split('+').collect::<Vec<&str>>();
@@ -32,9 +28,7 @@ pub fn parse(args: Vec<&str>) -> Result<Roll, ()>{
                 value_and_bonnus = splited_param[1].split('-').collect::<Vec<&str>>();
                 operator = "-";
             }
-            //assert!(value_and_bonnus[0].parse::<i8>().is_ok());
             value = value_and_bonnus[0].parse::<i8>().expect("");
-            //assert!(value_and_bonnus[1].parse::<i16>().is_ok());
             println!("{}", operator.to_string()+value_and_bonnus[1]);
             add = (operator.to_string()+value_and_bonnus[1]).parse::<i16>().expect("");
         }
